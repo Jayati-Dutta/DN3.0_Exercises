0b78785d7244ee1b79d59dace09964d05e188666
Jayati-Dutta <jayatidutta2003@gmail.com>
2024-08-16 15:01:14 +0530

    Delete Week_3_Exercises/Exercise7/EmployeeManagementSystem/src/main/java/com/jayati/EmployeeManagementSystemrepositoryDepartmentRepository.java

--- Week_3_Exercises/Exercise7/EmployeeManagementSystem/src/main/java/com/jayati/EmployeeManagementSystemrepositoryDepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.jayati.EmployeeManagementSystem.repository;
-
-import main.java.com.jayati.EmployeeManagementSystem.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-
-}
